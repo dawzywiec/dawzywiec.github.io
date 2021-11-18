0de979478688eb7ef38d2ea26f69cc32adf3980f
Dawid Zywiec <dawzywiec@gmail.com>
2021-11-18 21:55:37 +0100

    3-suma-iloczyn zadanie poprawione

--- 4-JavaScript/3-suma-iloczyn/js/script.js
+++ 4-JavaScript/3-suma-iloczyn/js/script.js
@@ -1,12 +1,13 @@
-let liczby = [1, 2, 3, 4, 5, 6];
-
-let suma = 0;
-let mnozenie = liczby[0];
-
-for (let i = 0; i < liczby.length; i++) {
-    suma += liczby[i];
-    mnozenie *= liczby[i];
+function dodawanieMnozenie(parametr) {
+    let dodawanie = 0;
+    let mnozenie = parametr[0];
+    for (let i = 0; i < parametr.length; i++) {
+        dodawanie += parametr[i];
+        mnozenie *= parametr[i];
+    };
+    console.log(dodawanie);
+    console.log(mnozenie);
 };
 
-console.log("Suma tablicy: " + suma);
-console.log("Mnozenie tablicy: " + mnozenie);
+let liczby = [1, 2, 3, 4, 5, 6];
+dodawanieMnozenie(liczby);
