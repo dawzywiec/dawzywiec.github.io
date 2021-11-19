aa923d6855ca3b2264b71fe7d79b5255a0c3807d
Dawid Zywiec <dawzywiec@gmail.com>
2021-11-19 12:52:37 +0100

    8-sortowanie-babelkowanie zadanie gotowe

---
+++ 4-JavaScript/8-sortowanie-babelkowanie/js/script.js
@@ -0,0 +1,17 @@
+let tablica = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
+console.log(tablica);
+
+function sortowanieTablicy(tablicaDoSortowania) {
+    for (let i = 0; i < tablicaDoSortowania.length; i++) {
+        for (let j = 0; j < tablicaDoSortowania.length; j++) {
+            if (tablicaDoSortowania[j] > tablicaDoSortowania[j + 1]) {
+                let temp = tablicaDoSortowania[j + 1];
+                tablicaDoSortowania[j + 1] = tablicaDoSortowania[j];
+                tablicaDoSortowania[j] = temp;
+            }
+        };
+    };
+    console.log(tablicaDoSortowania);
+};
+
+sortowanieTablicy(tablica);
