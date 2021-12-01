adf9191fbb1925cceccbdc295f318abbaf2ff647
Dawid Zywiec <dawzywiec@gmail.com>
2021-12-01 20:39:04 +0100

    1-pobierz-dane-programisty zadanie zakonczone

---
+++ 7-AJAX/1-pobierz-dane-programisty/js/script.js
@@ -0,0 +1,19 @@
+const btn = $('#przycisk');
+let pobranoDane = 0;
+
+btn.click(function () {
+    $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
+        .done(function (data) {
+            if (pobranoDane === 0) {
+                pobranoDane += 1;
+                console.log(data);
+                btn.after('<div id="dane-programisty">Pobrano dane: <ul><li>Imię: ' + data.imie + '</li><li>Nazwisko: ' + data.nazwisko + '</li><li>Zawód: ' + data.zawod + '</li><li>Firma: ' + data.firma + '</ul></div>');
+            } else {
+                $('div').remove();
+                btn.after('<div id="dane-programisty">Zaktualizowano dane: <ul><li>Imię: ' + data.imie + '</li><li>Nazwisko: ' + data.nazwisko + '</li><li>Zawód: ' + data.zawod + '</li><li>Firma: ' + data.firma + '</ul></div>');
+            };
+        })
+        .fail(function (error) {
+            console.log('Nie pobrano danych')
+        })
+})
